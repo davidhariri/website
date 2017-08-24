88d715d5603edc8ea292445ed4b1bbd79bd7b0ec
David Hariri <david.hariri@gmail.com>
2017-08-23 20:43:06 -0400

    dialing in styles

--- main.py
+++ main.py
@@ -10,24 +10,19 @@ app = Flask(__name__)
 def render_home():
     posts = [
         {
+            "id": 1,
             "text": {
-                "html": "<p><b>Hello 👋</b> I'm currently working on a new version of my site. It's going to be <i>great</i> when it's finished. Until then, you can follow along <a href=\"https://github.com/davidhariri/website/\">on Github</a> or talk to me <a href=\"https://twitter.com/davehariri/\">on Twitter</a>. 🖖</p>",
-                "md": "**Hello 👋** I'm currently working on a new version of my site. It's going to be *great* when it's finished. Until then, you can follow along [on Github](https://github.com/davidhariri/website) or talk to me [on Twitter](https://twitter.com/davehariri/). 🖖"
+                "html": "<p>I'm currently working on a new version of my site. It's going to be <i>great</i> when it's finished. Until then, you can follow along <a href=\"https://github.com/davidhariri/website/\">on Github</a> or talk to me <a href=\"https://twitter.com/davehariri/\">on Twitter</a>.</p>",
+                "md": "I'm currently working on a new version of my site. It's going to be *great* when it's finished. Until then, you can follow along [on Github](https://github.com/davidhariri/website) or talk to me [on Twitter](https://twitter.com/davehariri/)."
             },
-            "media": [
-                {
-                    "type": "image/png",
-                    "url": "https://ada.imgix.net/images/57487a7c-ba75-4ebf-abe8-fe5c14ee9f06.png",
-                    "size": [1536, 2048]
-                }
-            ],
+            "media": [],
             "location": {
                 "coordinates": [43.64920, -79.39720],
                 "friendly": "Toronto",
             },
             "created": {
                 "utc": "2017-08-23T19:35:43.511Z",
-                "friendly": "August 23 2017"
+                "friendly": "August 23rd 2017"
             }
         }
     ]
