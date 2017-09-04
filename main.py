bf6049ee43492d90f5b15fbbd33a3514630ba153
David Hariri <david.hariri@gmail.com>
2017-09-04 19:39:14 -0400

    Nicely formatted topic list under each Note that has one

--- main.py
+++ main.py
@@ -19,6 +19,31 @@ def render_date_ago(timestamp):
     return human(d, 1)
 
 
+@app.template_filter("topics")
+def render_topic_list(topics):
+    if len(topics) == 0:
+        return ""
+
+    t_str = " in "
+
+    for i, topic in enumerate(topics):
+        if i == 0:
+            t_str_join = ""
+        elif i == len(topics) - 1 and len(topics) > 1:
+            # Special case for when we get to the end of the list
+            t_str_join = " and "
+        else:
+            t_str_join = ", "
+
+        t_str_template = "{j}<a href=\"/notes/?topic={t}\" class=\"note__info__topic\">{t}</a>"
+        t_str += t_str_template.format(
+             t=topic,
+             j=t_str_join
+        )
+
+    return t_str
+
+
 @app.route("/")
 @app.route("/notes/")
 def render_home():
